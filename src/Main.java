a4124bdb3e5ec1deb6ff204f5284ef078bb20792
WeltMen <12110921@mail.sustech.edu.cn>
2022-05-22 20:30:37 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+
         SwingUtilities.invokeLater(() -> {
            Enter enter = new Enter(1000, 760);
             enter.setVisible(true);
